50dea2f95c17987c0a2625561cddb4b4bc3a0d80
Mario Alexandre dos Reis Rodrigues <marodrigues20@gmail.com>
2021-03-23 09:58:46 +0000

    fix: fix the description

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    "branches": ["master"],
+    repositoryUrl: "https://github.com/marodrigues20/react-app",
+    plugins: [
+        '@semantic-release/commit-analyzer', 
+        '@semantic-release/release-notes-generator', 
+        '@semantic-release/github' ]
+
+}
