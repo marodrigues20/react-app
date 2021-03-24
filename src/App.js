66ff7fb682aec984a67ecb3567070bf382a5c520
Mario Alexandre dos Reis Rodrigues <marodrigues20@gmail.com>
2021-03-24 22:59:24 +0000

    fix:Section5: Open Issue when the workflow fail

--- src/App.js
+++ src/App.js
@@ -15,11 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-<<<<<<< Updated upstream
           Learn 123
-=======
-          Learn
->>>>>>> Stashed changes
         </a>
       </header>
     </div>
