95bf7792522e60aff4e0cf9d4ad7e39b2010fbeb
Mario Alexandre dos Reis Rodrigues <marodrigues20@gmail.com>
2021-03-27 22:27:37 +0000

    Section5: My first JavaScript Github Actions

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -605,13 +605,25 @@ const github = __webpack_require__(469);
 
 try{
     //throw( new Error("some error message"));
+
+
+    core.debug('Debug message');
+    core.warning('Warning message');
+    core.debug('Error message');
+
+
     const name = core.getInput('who-to-greet');
+    core.setSecret(name);
     console.log('Hello ${{name}}');
     
     const time = new Date();
     core.setOutput("time", time.toTimeString());
     
+    core.startGroup('Logging github object');
     console.log(JSON.stringify(github, null, '\t'));
+    core.endGroup();
+
+    core.exportVariable('HELLO', 'hello');
 }catch(error){
     core.setFailed(error.message);
 }
