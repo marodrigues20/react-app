90f39d3d4aeeb35de24277d5e0527db5353d6d3e
Mario Alexandre dos Reis Rodrigues <marodrigues20@gmail.com>
2021-03-27 23:31:36 +0000

    Section5: My first JavaScript Github Actions

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -3545,7 +3545,7 @@ async function run(){
         const body = core.getInput('body');
         const assignees = core.getInput('assignees');
 
-        const octokit = new github.GitHub (token);
+        const octokit = new github.github(token);
 
         const response = await octokit.rest.issues.create({
             //owner: github.context.repo.owner,
