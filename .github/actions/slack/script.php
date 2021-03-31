2a2e5ae397d34ca4442b2ab5cabde8c9db175915
Mario Alexandre dos Reis Rodrigues <marodrigues20@gmail.com>
2021-03-31 20:26:06 +0100

    Docker Image with PHP - Slack

--- .github/actions/slack/script.php
+++ .github/actions/slack/script.php
@@ -3,56 +3,56 @@
 require_once 'vendor/autoload.php';
 Requests::register_autoloader();
 
-var_dump($argv);
-var_dump($_ENV);
+// var_dump($argv);
+// var_dump($_ENV);
 
-//echo "::debug ::Sending a request to slack\n";
+echo "::debug ::Sending a request to slack\n";
 
-//$response = Requests::post(
-//    $_ENV['INPUT_SLACK_WEBHOOK'],
-//    array(
-//        'Content-type' => 'application/json'
-//    ),
-//    json_encode(array (
-//        'blocks' => 
-//            array (
-//                array (
-//                    "type" => "section",
-//                    "text" => array (
-//                        "type" => "mrkdwn",
-//                        "text" => $_ENV['INPUT_MESSAGE'],
-//                    ),
-//                ),
-//                array (
-//                    "type" => "section",
-//                    "fields" => array (
-//                        array (
-//                            "type" => "mrkdwn",
-//                            "text" => "*Repository:*\n{$_ENV['GITHUB_REPOSITORY']}",
-//                        ),
-//                        array (
-//                            "type" => "mrkdwn",
-//                            "text" => "*Event:*\n{$_ENV['GITHUB_EVENT_NAME']}",
-//                       ),
-//                        array (
-//                            "type" => "mrkdwn",
-//                            "text" => "*Ref:*\n{$_ENV['GITHUB_REF']}",
-//                        ),
-//                        array (
-//                            "type" => "mrkdwn",
-//                            "text" => "*SHA:*\n{$_ENV['GITHUB_SHA']}",
-//                        ),
-//                    ),
-//                ),
-//            ),
-//    ))
-//);
+$response = Requests::post(
+    $_ENV['INPUT_SLACK_WEBHOOK'],
+    array(
+        'Content-type' => 'application/json'
+    ),
+    json_encode(array (
+        'blocks' => 
+            array (
+                array (
+                    "type" => "section",
+                    "text" => array (
+                        "type" => "mrkdwn",
+                        "text" => $_ENV['INPUT_MESSAGE'],
+                    ),
+                ),
+                array (
+                    "type" => "section",
+                    "fields" => array (
+                        array (
+                            "type" => "mrkdwn",
+                            "text" => "*Repository:*\n{$_ENV['GITHUB_REPOSITORY']}",
+                        ),
+                        array (
+                            "type" => "mrkdwn",
+                            "text" => "*Event:*\n{$_ENV['GITHUB_EVENT_NAME']}",
+                        ),
+                        array (
+                            "type" => "mrkdwn",
+                            "text" => "*Ref:*\n{$_ENV['GITHUB_REF']}",
+                        ),
+                        array (
+                            "type" => "mrkdwn",
+                            "text" => "*SHA:*\n{$_ENV['GITHUB_SHA']}",
+                        ),
+                    ),
+                ),
+            ),
+    ))
+);
 
-//echo "::group::Slack Reponse\n";
-//var_dump($response);
-//echo "::endgroup::\n";
+echo "::group::Slack Reponse\n";
+var_dump($response);
+echo "::endgroup::\n";
 
-//if(!$response->success) {
-//    echo $response->body;
-//    exit(1);
-//}
+if(!$response->success) {
+    echo $response->body;
+    exit(1);
+}
